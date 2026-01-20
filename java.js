bcc5c14e81886a8b0cc2e2d7d378e4b55636da26
ChEeS123 <143178139+panir123@users.noreply.github.com>
2026-01-20 02:51:25 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,100 @@
+const first=document.getElementById("txt");
+const ofbg=document.getElementById("ofbg");
+const second=document.getElementById("tt");
+const ofg=document.getElementById("ofg");
+const third=document.getElementById("ttk");
+const ofc=document.getElementById("ofc");
+const Four=document.getElementById("tttk");
+const oc=document.getElementById("oc");
+const Five=document.getElementById("tk");
+const ocs=document.getElementById("ocs");
+const Six=document.getElementById("tik");
+const poc=document.getElementById("poc");
+const Seven=document.getElementById("tiks");
+const pocs=document.getElementById("pocs");
+const Eigth=document.getElementById("tiktoks");
+const poses=document.getElementById("poses");
+const Nine=document.getElementById("tiktok");
+const pose=document.getElementById("pose");
+const Ten=document.getElementById("pipi");
+const picnic=document.getElementById("picnic");
+
+first.addEventListener('mouseenter', function() {
+  ofbg.classList.add('show');
+});
+
+first.addEventListener('mouseleave', function() {
+  ofbg.classList.remove('show');
+});
+
+second.addEventListener('mouseenter', function() {
+  ofg.classList.add('show');
+});
+
+second.addEventListener('mouseleave', function() {
+  ofg.classList.remove('show');
+});
+
+third.addEventListener('mouseenter', function() {
+  ofc.classList.add('show');
+});
+
+third.addEventListener('mouseleave', function() {
+  ofc.classList.remove('show');
+});
+
+Four.addEventListener('mouseenter', function() {
+  oc.classList.add('show');
+});
+
+Four.addEventListener('mouseleave', function() {
+  oc.classList.remove('show');
+});
+
+Five.addEventListener('mouseenter', function() {
+  ocs.classList.add('show');
+});
+
+Five.addEventListener('mouseleave', function() {
+  ocs.classList.remove('show');
+});
+
+Six.addEventListener('mouseenter', function() {
+  poc.classList.add('show');
+});
+
+Six.addEventListener('mouseleave', function() {
+  poc.classList.remove('show');
+});
+
+Seven.addEventListener('mouseenter', function() {
+  pocs.classList.add('show');
+});
+
+Seven.addEventListener('mouseleave', function() {
+  pocs.classList.remove('show');
+});
+
+Eigth.addEventListener('mouseenter', function() {
+  poses.classList.add('show');
+});
+
+Eigth.addEventListener('mouseleave', function() {
+  poses.classList.remove('show');
+});
+
+Nine.addEventListener('mouseenter', function() {
+  pose.classList.add('show');
+});
+
+Nine.addEventListener('mouseleave', function() {
+  pose.classList.remove('show');
+});
+
+Ten.addEventListener('mouseenter', function() {
+  picnic.classList.add('show');
+});
+
+Ten.addEventListener('mouseleave', function() {
+  picnic.classList.remove('show');
+});
